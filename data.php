149eacecef91ac1c7169392d1ee21e159c090396
louiscocks <99043889@mydavinci.nl>
2017-04-12 09:52:15 +0200

    update.MadLibs

---
+++ data.php
@@ -0,0 +1,53 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Mad Libs</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="madmad.css">
+</head>
+<body>
+
+<div id="block">
+   
+    <h1>Mad Libs</h1>
+
+    <ul>
+	    <li><a href="madmad.html">Er heerst paniek...</a></li>
+	    <li><a href="onkunde.html">Onkunde</a></li>
+    </ul>
+
+    <div class="content">
+        <h2>Er heerst paniek...</h2>
+
+		  <p>Erst heerst paniek in koningrik <?php echo $_GET["three"]; ?>. Koning <?php echo $_GET["six"]; ?> is ten einde raad en als koning <?php echo $_GET["six"]; ?> ten einde raad is, dan roept hij zijn ten-einde-raadsheer <?php echo $_GET["two"]; ?>.</p>
+
+		  <p>" <?php echo $_GET["two"]; ?> ! Het is een ramp! Het is een schande!"</p>
+
+
+		  <P>"Sire, Majesteit, Uwe Luidruchtigheid, wat is er aan hand?"</P>
+
+
+		  <P>"Mijn <?php echo $_GET["one"]; ?> is verdwenen! Zo maar, zonder waarschuwing. En ik had net <?php echo $_GET["five"]; ?> voor hem gekocht!"</P>
+
+
+		  <p>"Majesteit, uw <?php echo $_GET["one"]; ?> komt vast vanzelf weer terug?"</p>
+
+
+		  <p>"Ja, da's leuk en aardig, maar hoe moet ik in de tussentijd <?php echo $_GET["eight"]; ?> leren?"</p>
+
+
+		  <p>"Maar Sire, daar kunt u toch uw <?php echo $_GET["seven"]; ?> voor gebruiken."</p>
+
+
+		  <p>"<?php echo $_GET["two"]; ?>, je hebt helemaal gelijk! Wat zou ik doen als ik jou niet had."</p>
+
+
+		  <p>"Mij <?php echo $_GET["four"]; ?>, Sire."</p>
+	</div>
+
+<footer><p>Deze website is gemaakt door Kareem.</p></footer>
+
+</div>
+</body>
+</html>
---
+++ data2.php
@@ -0,0 +1,32 @@
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Mad Libs</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="madmad.css">
+</head>
+<body>
+
+<div id="block">
+   
+    <h1>Mad Libs</h1>
+
+    <ul>
+	    <li><a href="madmad.html">Er heerst paniek...</a></li>
+	    <li><a href="onkunde.html">Onkunde</a></li>
+    </ul>
+
+    <div class="content">
+        <h2>Onkunde</h2>
+
+		  <p>Er zijn veel mensen die niet kunnen <?php echo $_GET["one"]; ?>. Neem nou <?php echo $_GET["two"]; ?>. Zelfs met de hulp van een <?php echo $_GET["four"]; ?> of zelfs <?php echo $_GET["three"]; ?> kan <?php echo $_GET["two"]; ?> niet <?php echo $_GET["one"]; ?>. Dat heeft niet te maken met een gebrek aan <?php echo $_GET["five"]; ?>, maar met een te veel aan <?php echo $_GET["six"]; ?>. Te veel <?php echo $_GET["six"]; ?> leidt tot <?php echo $_GET["seven"]; ?> en dat is niet goed als je wilt tekenen. Helaas voor <?php echo $_GET["two"]; ?>.</p>
+
+	</div>
+
+<footer><p>Deze website is gemaakt door Kareem.</p></footer>
+
+</div>
+</body>
+</html>
